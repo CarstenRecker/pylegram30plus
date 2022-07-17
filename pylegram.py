e4feadc9ddca4cef865c2757395f76a1afc23886
Carsten Recker <108832359+CarstenRecker@users.noreply.github.com>
2022-07-17 19:29:20 +0200

    test

--- pylegram.py
+++ pylegram.py
@@ -17,6 +17,26 @@ async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
+async def greet_chat_members(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Greets new users in chats and announces when someone leaves"""
+    result = extract_status_change(update.chat_member)
+    if result is None:
+        return
+
+    was_member, is_member = result
+    cause_name = update.chat_member.from_user.mention_html()
+    member_name = update.chat_member.new_chat_member.user.mention_html()
+
+    if not was_member and is_member:
+        await update.effective_chat.send_message(
+            f"{member_name} was added by {cause_name}. Welcome!",
+            parse_mode=ParseMode.HTML,
+        )
+    elif was_member and not is_member:
+        await update.effective_chat.send_message(
+            f"{member_name} is no longer with us. Thanks a lot, {cause_name} ...",
+            parse_mode=ParseMode.HTML,
+        )
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('5555055013:AAGOrmUaHGybA_P7REUtp1zmtWZnLJys8mU').build()
@@ -25,8 +45,9 @@ if __name__ == '__main__':
     start_handler = CommandHandler('start', start)
     caps_handler = CommandHandler('caps', caps)
     echo_handler = CommandHandler('echo', echo)
+    ChatMemberHandler = CommandHandler('greet', greet)
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
     application.add_handler(caps_handler)
-
+    application.add_handler(ChatMemberHandler(greet_chat_members, ChatMemberHandler.CHAT_MEMBER))
     application.run_polling()
