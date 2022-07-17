a4875ac5e0582261ff28faff7d38f4ce92255059
Carsten Recker <108832359+CarstenRecker@users.noreply.github.com>
2022-07-17 18:15:22 +0200

    bla

--- pylegram.py
+++ pylegram.py
@@ -1,1 +1,13 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+app = ApplicationBuilder().token("5555055013:AAGOrmUaHGybA_P7REUtp1zmtWZnLJys8mU").build()
+
+app.add_handler(CommandHandler("hello", hello))
+
+app.run_polling()
