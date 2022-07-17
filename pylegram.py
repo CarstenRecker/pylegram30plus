a230b32ba5515049f230bbe139dc78a405f7bbe4
Carsten Recker <108832359+CarstenRecker@users.noreply.github.com>
2022-07-17 19:03:24 +0200

    cahbnng

--- pylegram.py
+++ pylegram.py
@@ -10,10 +10,23 @@ logging.basicConfig(
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token('5555055013:AAGOrmUaHGybA_P7REUtp1zmtWZnLJys8mU').build()
     
+
     start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    caps_handler = CommandHandler('caps', caps)
     
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(caps_handler)
+
     application.run_polling()
